dfa62c1c22efa5f1311578cfbb41ab0ddfb7a056
Tanmay Khandelwal <tkayathwal@gmail.com>
2026-07-31 16:24:25 +0530

    Update pipe_MIPS32.v

--- pipe_MIPS32.v
+++ pipe_MIPS32.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.06.2026 00:46:18
-// Design Name: 
-// Module Name: pipe_MIPS32
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module pipe_MIPS32 (clk1, clk2);
   input clk1, clk2;
